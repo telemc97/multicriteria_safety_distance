f1497010f577a9878c370ed5cb6b1617f8a6c9b1
telemc97 <telemc97@gmail.com>
2023-03-13 18:24:13 +0200

    WIP TODO: decrease resolution to 0.1m

--- src/criteria_map.py
+++ src/criteria_map.py
@@ -80,17 +80,20 @@ class criteria_map:
     def get_average_slope(self, x,y):
         if (self.map[x,y]['det_sum']>3):
             rospy.loginfo(self.map[x,y]['det_sum'])
-            sum_set = int((self.map[x,y]['points'].shape[0]//3) * 3)
+            sum_set = int((self.map[x,y]['points'].shape[0]//3))
+            det_sum = 0
             det_array = np.zeros(shape=(sum_set,3), dtype=np.float16)
             for i in range(sum_set):
-                pointxyz = self.get_points_from_cantor(self.map[x,y]['points'][i])
-                det_array[i,:] = pointxyz
-            det = self.det_calc(det_array)
-
-            return det
+                for j in range(3):
+                    pointxyz = self.get_points_from_cantor(self.map[x,y]['points'][i*3+j])
+                    det_array[j,:] = pointxyz
+                det = self.det_calc(det_array)
+                det_sum += det
+            avg_det = det_sum/sum_set
+            return avg_det
         else:
-            det = 0
-            return det
+            avg_det = 0
+            return avg_det
 
 
     def point_to_index(self, point):
@@ -132,12 +135,12 @@ class criteria_map:
         detectedPoints = self.map[x, y]['points']
         it = np.nditer(detectedPoints, op_flags=['readonly'])
         for x in it:
-            if (int(x/self.comparRes)==cantorPoint): new_point = False
+            if (self.get_cantor_hash(x)==cantorPoint): new_point = False
             rospy.loginfo('Point: %f, %f, %f already detected' % (point[0], point[1], point[2]))
         return new_point
 
     
-
+    #TODO decrease resolution
     def get_cantor_hash(self, point) -> np.uint64:
         newPoints = np.zeros(shape=(3), dtype=np.int64)
         for i in range(3):
@@ -149,7 +152,6 @@ class criteria_map:
             meters = np.uint64(point[i] - point[i]%1)
             centimeters = np.uint64(point[i]%1*100)
             newPoints[i] = np.uint64((negative*100000) + (meters*100) + centimeters)
-        print(newPoints)
         a = np.uint64((math.pow(newPoints[0],2) + newPoints[0] + 2*newPoints[0]*newPoints[1] + 3*newPoints[1] + math.pow(newPoints[1],2))/2)
         cHash = np.uint64((math.pow(a,2) + a + 2*a*newPoints[2] + 3*newPoints[2] + math.pow(newPoints[2],2))/2)
         return cHash
@@ -158,12 +160,12 @@ class criteria_map:
     def get_points_from_cantor(self, cantor: np.uint64):
         w = np.uint64(math.floor((math.sqrt(8*cantor+1)-1)/2))
         t = np.uint64((math.pow(w,2)+w)/2)
-        z = cantor - t
-        xy = w - z
+        z = np.uint64(cantor - t)
+        xy = np.uint64(w - z)
         xyw = np.uint64(math.floor((math.sqrt(8*xy+1)-1)/2))
         xyt = np.uint64((math.pow(xyw,2)+xyw)/2)
-        y = xy - xyt
-        x = xyw - y
+        y = np.uint64(xy - xyt)
+        x = np.uint64(xyw - y)
         newPoints = np.array([x,y,z], dtype=np.int64)
         point = np.zeros(shape=(3), dtype=np.float32)
         for i in range(3):
